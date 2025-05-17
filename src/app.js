cc08e03d0545579efa5fa9abf8704f9de7f2e4d6
shyam <Shyamkumar9645@gmail.com>
2025-05-17 21:16:04 +0530

    cors issue

--- src/app.js
+++ src/app.js
@@ -20,11 +20,10 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: ['https://tiny-semifreddo-fdd2a6.netlify.app', 'http://localhost:3000'],
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type, Authorization',
   credentials: true
-
 }));
 
 // Request logging
