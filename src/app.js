991d298988ea1feedfd336ba765dbcfe1418441a
shyam <Shyamkumar9645@gmail.com>
2025-05-18 13:17:25 +0530

    updated render url

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS configuration
 app.use(cors({
-  origin: ['https://personal-finance-tracker-54et.onrender.com', 'http://localhost:3000'],
+  origin: ['https://personal-finance-tracker-ia63.onrender.com', 'http://localhost:3000'],
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type, Authorization',
   credentials: true
